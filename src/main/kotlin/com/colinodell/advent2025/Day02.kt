870bb84ba90c0f095d25b57a9aba223143f0a9af
Colin O'Dell <codell@seatgeek.com>
2025-12-02 12:04:06 -0500

    Optimize Day02
    It's probably more efficient to be lazy

--- src/main/kotlin/com/colinodell/advent2025/Day02.kt
+++ src/main/kotlin/com/colinodell/advent2025/Day02.kt
@@ -6,8 +6,8 @@ class Day02(input: String) {
         LongRange(start, end)
     }
 
-    private val regexPart1 = Regex("""^(\d+)\1$""")
-    private val regexPart2 = Regex("""^(\d+)\1+$""")
+    private val regexPart1 = Regex("""^(\d+?)\1$""")
+    private val regexPart2 = Regex("""^(\d+?)\1+$""")
 
     fun solvePart1() = productIds.filter { regexPart1.matches(it.toString()) }.sum()
     fun solvePart2() = productIds.filter { regexPart2.matches(it.toString()) }.sum()
